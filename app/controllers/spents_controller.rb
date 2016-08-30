bf114a9461c18011bbf7fe567ab93a17a8440d6b
sqacecadet <sqacecadet@gmail.com>
2016-08-30 00:54:13 -0300

    BDD to create spents with Tests

--- app/controllers/spents_controller.rb
+++ app/controllers/spents_controller.rb
@@ -4,4 +4,25 @@ class SpentsController < ApplicationController
 
   end
 
+  def new
+    @spent = Spent.new
+  end
+
+  def create
+    @spent = Spent.new(params.require(:spent).permit(:value, :date, :description))
+    @spent.save
+    redirect_to @spent
+  end
+
+  def show 
+    @spent = Spent.find(params[:id])
+  end
+  
+  private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def spent_params
+    params.require(:spent).permit(:value, :date, :description)
+  end
+  
 end
--- spec/features/spent_spec.rb
+++ spec/features/spent_spec.rb
@@ -12,6 +12,24 @@ describe 'navigate' do
         visit spents_path
         expect(page).to have_content(/Spents/)
 	  end
-
 	end
+
+    describe 'creation' do 
+      it 'has a form that can be reached' do 
+        visit new_spent_url
+        expect(page.status_code).to eq(200)
+      end	
+
+      it 'allows users to create a new topic from the /new page' do
+        visit new_spent_url
+
+        fill_in 'spent[value]', with: "19.90"
+        fill_in 'spent[date]', with: Date.today
+        fill_in 'spent[description]', with: "Star Wars Is a great movie"
+
+        click_on "Save"
+
+        expect(page).to have_content("Star Wars Is a great movie")
+      end
+    end
 end
