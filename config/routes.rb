078d69a68d245d4270e6e8d995cf3f3c27ce24d8
sqacecadet <sqacecadet@gmail.com>
2016-09-12 21:47:34 -0300

    Blocking non Admin User to see the admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :users
-    #Parece que o administrate tem um bug com classes com STI Single table inhreitance
-    # resources :admin_users
+    resources :admin_users
     resources :spents
     resources :wallets
     root to: "users#index"
