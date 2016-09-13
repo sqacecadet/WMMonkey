a5ba00695b0c0aa442070da100db9d782f5b4221
Anderson Silva <sqacecadet@gmail.com>
2016-09-13 13:48:20 -0300

    Implemented enum type for status in spents

--- app/models/spent.rb
+++ app/models/spent.rb
@@ -1,4 +1,9 @@
 class Spent < ApplicationRecord
+        enum status: { submitted: 0, approved: 1, rejected: 2 }
 	belongs_to :user
 	validates_presence_of :value, :date, :description
 end
+#rails g migration add_status_to_spents status:integer default = 0
+#rake db:migrate
+#check the schema
+ 
